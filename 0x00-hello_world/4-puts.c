ed47817407bd7605f5f725234f535d1de7bd892b
very-true <agbekotony10@gmail.com>
2023-03-16 09:16:58 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,14 +4,13 @@
 /**
  * main - Main function of my project
  *
- * Return: return 0 everytime
+ * Return: returns 0 everytime
  */
 int main(void)
 {
-  char displayed[52];
+	char displayed[52];
 
-  strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-  puts(displayed);
-  return (0);
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
+	return (0);
 }
-
