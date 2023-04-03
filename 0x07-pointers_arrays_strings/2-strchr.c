8f0b9bae7f2ccbcbfad450d2b23833a1332d2b1e
very-true <agbekotony10@gmail.com>
2023-04-03 16:17:12 -0700

    tasks

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - black
+ * @s: green
+ * @c: red
+ * Return: *S
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+return (0);
+}
+
