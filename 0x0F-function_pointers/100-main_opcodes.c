39b78307156c66f96d0a0937331c96a5e3c03e3d
very-true <agbekotony10@gmail.com>
2023-04-19 17:49:29 -0700

    tasks

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_opcodes - aaaa
+ * @a: bbbb
+ * @n: cccc
+ * Return: dddd
+ */
+
+void print_opcodes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+}
+
+/**
+ * main - ddd
+ * @argc: cccc
+ * @argv: bbbb
+ * Return: aaaa
+ */
+
+int main(int argc, char **argv)
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
+}
